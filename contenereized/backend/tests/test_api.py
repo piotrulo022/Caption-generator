008e11cbaed56ed3f8c5824a89c8d9f3b8ba1700
Piotr Szyszka <piotrulo022@gmail.com>
2024-04-14 22:06:40 +0200

    finished tests for every endpoint

--- contenereized/backend/tests/test_api.py
+++ contenereized/backend/tests/test_api.py
@@ -1,12 +1,14 @@
-from fastapi.testclient import TestClient
 import sys
+import sqlite3
+from fastapi.testclient import TestClient
 
 sys.path.append("..")
 from api import app
 
+
 client = TestClient(app)
 
-# Get methods
+# GET methods
 def test_read_root():
     response = client.get('/')
     assert response.status_code == 200
@@ -17,33 +19,83 @@ def test_get_model_name():
     assert response.status_code == 200
     assert response.json()['model_name'] is not None
 
+# POST methods
 
 def test_predict_url():
     response = client.post('/predict_image_url/',
                             json = {'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png',
-                                    'prompt': 'ss',
-                                    'push_db': False})
+                            'push_db': False})
     
     assert response.status_code == 200
 
 def test_predict_file():
-    from PIL import Image
-    from io import BytesIO
+    image = open("elephantoo.png", "rb")
 
-    with open("elephantoo.png", "rb") as file:
-        bytes_data = BytesIO(file.read())
-
-    filename = getattr(bytes_data, 'name', None)
+    files = {'file': ('elephant', image, 'image/png')}
+    response = client.post('/predict_image_file/', files = files, json = {'push_db': False})
     
-    image = Image.open(bytes_data)
-    filetype = image.format
-
-
-    files = {'file': (filename, bytes_data, filetype)}
-
-    response = client.post('/predict_image_file/', files = files,
-                                                json = {'prompt': 'ss',
-                                                'push_db': False})
-    
-
     assert response.status_code == 200
+
+
+# DELETE methods
+
+def test_purge_database():
+
+    def delete_table(): # create dummy database and delete images exactly like in API
+        conn = sqlite3.connect(':memory:')
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Images (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            caption TEXT,
+                            model_used TEXT,
+                            image_file BLOB NOT NULL,
+                            created_time TEXT DEFAULT CURRENT_TIMESTAMP
+                        )''')
+        try:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM Images;")
+            conn.commit()
+            cursor.close()
+            return 'success'
+        except sqlite3.Error as err:
+            return f"failed to delete row {str(err)}"
+    
+
+    result = delete_table()
+
+    assert result == 'success'
+
+
+
+def test_delete_row():
+
+    def delete_row():
+        conn = sqlite3.connect(':memory:')
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Images (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            caption TEXT,
+                            model_used TEXT,
+                            image_file BLOB NOT NULL,
+                            created_time TEXT DEFAULT CURRENT_TIMESTAMP
+                        )''')
+        try:
+            # conditions for rows to be deleted
+            created_time = "2024-04-14 19:08:54"
+            caption = "foo"
+
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM Images WHERE caption = ? AND created_time = ?", (caption, created_time))
+     
+            conn.commit()
+            cursor.close()
+            return 'success'
+        except sqlite3.Error as err:
+            return f"failed to delete row {str(err)}"
+    
+
+    result = delete_row()
+
+    assert result == 'success'
+
+
