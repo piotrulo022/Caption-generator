2b862f2de507d4666ca4dd622fec92b7855db8da
Piotr Szyszka <piotrulo022@gmail.com>
2024-04-16 19:56:22 +0200

    Revert "added homepage contents"
    This reverts commit 6f8709de4cf34b4769fbea893b243df6191467fb.

--- contenereized/streamlit_app/homepage.py
+++ contenereized/streamlit_app/homepage.py
@@ -3,23 +3,11 @@ from base64 import b64encode
 
 def app():
     st.title('Home page')
-    st.markdown(
-                """                
-                This application serves to generate descriptions for images using AI models provided by [the huggingface transformers library](https://huggingface.co/docs/transformers/). It consists of three functional tabs:
-                
-                - ***Config*** - tab delivering user an ability to change the model used for caption generating. Currently supported models are [tarekziade/deit-tiny-distilgpt2](https://huggingface.co/tarekziade/deit-tiny-distilgpt2), [Salesforce/blip-image-captioning-base](https://huggingface.co/Salesforce/blip-image-captioning-base), [llava-hf/llava-1.5-7b-hf](https://huggingface.co/llava-hf/llava-1.5-7b-hf), [noamrot/FuseCap_Image_Captioning](https://huggingface.co/noamrot/FuseCap_Image_Captioning)
-                                
-                - ***Generate captions***: Here you can obtain descriptions for images you provide. You can upload a file from their local disk or provide a URL to a webpage containing the image. Also you have the option to save images along with their descriptions in the database.
+    st.write("Here will be info and descriptions how to use application")
 
-                - ***Database***: Here you can view saved images and manage them by deleting individual entries or clearing the entire database using the provided buttons. 
-                """
-                )
-    
-    
-    st.markdown('If you find any issue, please do not hesitate and share it on issues page on project repository.')
     html_content = """
     <div style="display: flex; justify-content: center;">
-        <a href="https://github.com/piotrulo022/Caption-generator/">
+        <a href="https://github.com/piotrulo022/">
             <img src="data:image/jpeg;base64,{}" width="128">
         </a>
     </div>
